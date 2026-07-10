1c7c4b541f6d826652aa8aee83d03171dbf712da
MuratGundo <94074054+MuratGundo@users.noreply.github.com>
2026-07-10 04:06:40 +0300

    Fix core namespace reference in analysis context

--- src/UCrew.TTARCH2.Core/Analysis/AnalysisContext.cs
+++ src/UCrew.TTARCH2.Core/Analysis/AnalysisContext.cs
@@ -4,13 +4,13 @@ namespace UCrew.TTARCH2.Core.Analysis;
 
 public sealed class AnalysisContext
 {
-    public AnalysisContext(ArchiveContext archive, ArchiveModel model)
+    public AnalysisContext(UCrew.TTARCH2.Core.ArchiveContext archive, ArchiveModel model)
     {
         Archive = archive ?? throw new ArgumentNullException(nameof(archive));
         Model = model ?? throw new ArgumentNullException(nameof(model));
     }
 
-    public ArchiveContext Archive { get; }
+    public UCrew.TTARCH2.Core.ArchiveContext Archive { get; }
 
     public ArchiveModel Model { get; }
 }
