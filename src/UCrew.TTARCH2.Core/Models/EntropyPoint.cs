3fc3582651ec6373f4847de79de53542b6c0cb68
MuratGundo <94074054+MuratGundo@users.noreply.github.com>
2026-07-10 03:38:26 +0300

    Add entropy point model

---
+++ src/UCrew.TTARCH2.Core/Models/EntropyPoint.cs
@@ -0,0 +1,10 @@
+namespace UCrew.TTARCH2.Core.Models;
+
+public sealed class EntropyPoint
+{
+    public long Offset { get; init; }
+
+    public int BlockSize { get; init; }
+
+    public double Value { get; init; }
+}
